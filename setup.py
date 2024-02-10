3859a5bd09cef28e6aabe9ffbcc1ed663aef127f
Edgar Salazar <edgarmsc@arizona.edu>
2024-02-10 10:31:36 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import re
+from setuptools import setup, find_packages
+
+project_name = 'dynbahlss'
+sources = glob.glob(os.path.join(project_name,'*.py'))
+
+# auto-updating version code taken from RadVel
+def get_property(prop, project):
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + "/__init__.py").read(),
+    )
+    return result.group(1)
+
+
+def get_requires():
+    reqs = []
+    for line in open("requirements.txt", "r").readlines():
+        reqs.append(line)
+    return reqs
+
+
+dist = setup(
+    name=project_name,
+    autor="Edgar Salazar",
+    author_email="edgarmsc@arizona.edu",
+    version=get_property("__version__", "physhalo"),
+    description="Modules for reproducing Salazar et. al. (2024) arXiv:####",
+    license="MIT License",
+    # url="http://github.com/edgarmsalazar/XXXX",
+    packages=find_packages(),
+    package_data={project_name: sources},
+    install_requires=get_requires(),
+    tests_require=['pytest'],
+)
