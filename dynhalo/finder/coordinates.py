cf5f0f6818dcc43bb49e7a5530cc6eb828c9e6c1
Edgar Salazar <edgarmsc@arizona.edu>
2024-03-15 22:15:23 -0700

    added computation of radial and tangential velocities

--- dynhalo/finder/coordinates.py
+++ dynhalo/finder/coordinates.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 
@@ -31,8 +33,53 @@ def relative_coordinates(
     return x - x0
 
 
-def rec_to_sph_coordinates(rec: np.ndarray):
-    return
+def get_vr_vt_from_coordinates(
+    pos: np.ndarray,
+    vel: np.ndarray,
+) -> Tuple[np.ndarray]:
+    """Computes the radial and tangential velocites from cartesian/rectangular 
+    coordinates.
+
+    Parameters
+    ----------
+    pos : np.ndarray
+        Cartesian coordinates
+    vel : np.ndarray
+        Cartesian velocities
+
+    Returns
+    -------
+    Tuple[np.ndarray]
+        Radial velocity, tangential velocity and magnitude squared of the velocity
+    """
+    # Transform coordinates from cartesian to spherical
+    #   rs = sqrt( x**2 + y**2 + z**2 )
+    rps = np.sqrt(np.sum(np.square(pos), axis=1))
+    #   theta = arccos( z / rs )
+    thetas = np.arccos(pos[:, 2] / rps)
+    #   phi = arctan( y / x)
+    phis = np.arctan2(pos[:, 1], pos[:, 0])
+
+    # Get radial vector in cartesian coordinates
+    rp_hat = np.zeros_like(pos)
+    rp_hat[:, 0] = np.sin(thetas) * np.cos(phis)
+    rp_hat[:, 1] = np.sin(thetas) * np.sin(phis)
+    rp_hat[:, 2] = np.cos(thetas)
+
+    # Get tangential vector in cartesian coordinates
+    rt_hat = np.zeros_like(pos)
+    rt_hat[:, 0] = np.cos(thetas) * np.cos(phis)
+    rt_hat[:, 1] = np.cos(thetas) * np.sin(phis)
+    rt_hat[:, 2] = -np.sin(thetas)
+
+    # Compute radial velocity as v dot r_hat
+    vr = np.sum(vel * rp_hat, axis=1)
+    vt = np.sum(vel * rt_hat, axis=1)
+    # Velocity squared
+    v2 = np.sum(np.square(vel), axis=1)
+
+    return vr, vt, v2
+
 
 if __name__ == "__main__":
     pass
