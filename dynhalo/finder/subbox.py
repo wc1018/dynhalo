5675aa2e1d73ff78f27e03971fb2815bbaa5c7e1
Edgar Salazar <edgarmsc@arizona.edu>
2024-03-15 18:18:12 -0700

    working for seeds and particles

--- dynhalo/finder/subbox.py
+++ dynhalo/finder/subbox.py
@@ -60,7 +60,7 @@ def generate_sub_box_grid(
     # Number of sub-boxes per side
     boxes_per_side = np.int_(np.ceil(boxsize / subsize))
 
-    # Determine data type for integer arrays based on the maximum number of 
+    # Determine data type for integer arrays based on the maximum number of
     # elements
     uint_dtype = get_np_unit_dytpe(boxes_per_side)
     # Set of natural numbers from 0 to N-1
@@ -111,7 +111,7 @@ def get_sub_box_id(
     """
     # Number of sub-boxes per side
     boxes_per_side = np.int_(np.ceil(boxsize / subsize))
-    # Determine data type for integer arrays based on the maximum number of 
+    # Determine data type for integer arrays based on the maximum number of
     # elements
     uint_dtype = get_np_unit_dytpe(boxes_per_side**2)
     # Shift in each dimension for numbering sub-boxes
@@ -177,7 +177,7 @@ def generate_sub_box_ids(
     Parameters
     ----------
     positions : np.ndarray
-        _description_
+        Cartesian coordinates
     boxsize : float
         Size of simulation box
     subsize : float
@@ -197,7 +197,7 @@ def generate_sub_box_ids(
     n_items = positions.shape[0]
     n_iter = n_items // chunksize
 
-    # Determine data type for integer arrays based on the maximum number of 
+    # Determine data type for integer arrays based on the maximum number of
     # elements
     boxes_per_side = np.int_(np.ceil(boxsize / subsize))
     uint_dtype = get_np_unit_dytpe(boxes_per_side**3)
@@ -205,15 +205,19 @@ def generate_sub_box_ids(
     ids = np.zeros(n_items, dtype=uint_dtype)
 
     for chunk in tqdm(range(n_iter), desc='Chunk', ncols=100, colour='blue'):
+        low = chunk * chunksize
         if chunk < n_iter - 2:
-            low = chunk * chunksize
             upp = (chunk + 1) * chunksize
-            ids[low:upp] = get_sub_box_id(positions[low:upp], boxsize, subsize)
         else:
-            low = chunk * chunksize
-            ids[low:] = get_sub_box_id(positions[low:], boxsize, subsize)
-    with h5.File(path+f'sub_box_id{name}.hdf5', 'w') as hdf:
-        hdf.create_dataset('ID', data=ids, dtype=uint_dtype)
+            upp = None
+        ids[low:upp] = get_sub_box_id(positions[low:upp], boxsize, subsize)
+
+    if name:
+        file_name = f'sub_box_id_{name}.hdf5'
+    else:
+        file_name = f'sub_box_id.hdf5'
+    with h5.File(path + file_name, 'w') as hdf:
+        hdf.create_dataset('SBID', data=ids, dtype=uint_dtype)
 
     return None
 
@@ -221,21 +225,113 @@ def generate_sub_box_ids(
 @timer
 def split_simulation_into_sub_boxes(
     positions: np.ndarray,
+    velocities: np.ndarray,
+    ids: np.ndarray,
     boxsize: float,
     subsize: float,
     chunksize: float,
+    dtypes: list,
     path: str,
+    name: str = None,
 ) -> None:
-    # Create directory if it does not exist
-    if not os.path.exists(path + 'sub_boxes'):
-        os.makedirs(path + 'sub_boxes')
-    
-    # uint_dtype = get_np_unit_dytpe(boxes_per_side**3)
+    # Check chunksize is smaller than the number of items
     n_items = positions.shape[0]
+    if chunksize > n_items:
+        raise ValueError(
+            f"The specified chunksize {chunksize} is larger than the number of items {n_items}")
+
+    # Create directory if it does not exist
+    save_path = path + 'sub_boxes/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    if not os.path.exists(save_path):
+        generate_sub_box_ids(positions, boxsize, subsize, chunksize, path, name)
+    
+    if name:
+        sub_box_ids_file = path + f'sub_box_id_{name}.hdf5'
+    else:
+        sub_box_ids_file = path + f'sub_box_id.hdf5'
+    with h5.File(sub_box_ids_file, 'r') as hdf:
+        sub_box_ids = hdf['SBID'][()]
+    
     n_iter = n_items // chunksize
 
-    
+    uint_dtype_row = get_np_unit_dytpe(n_items)
+    row_idx = np.arange(n_items, dtype=uint_dtype_row)
+
+    uint_dtype_ids = get_np_unit_dytpe(np.max(ids))
+    for chunk in tqdm(range(n_iter), desc='Chunk', ncols=100, colour='blue'):
+        # Select chunk
+        low = chunk * chunksize
+        if chunk < n_iter - 2:
+            upp = (chunk + 1) * chunksize
+        else:
+            upp = None
+        sb_ids = sub_box_ids[low:upp]
+        pos = positions[low:upp]
+        vel = velocities[low:upp]
+        pid = ids[low:upp]
+        row = row_idx[low:upp]
+
+        sb_unique = np.unique(sb_ids)
+        order = np.argsort(sb_ids)
+        for item in sb_unique:
+            left = np.searchsorted(sb_ids, item, side="left", sorter=order)
+            right = np.searchsorted(sb_ids, item, side="right", sorter=order)
+
+            pos_item = pos[order][left:right]
+            vel_item = vel[order][left:right]
+            pid_item = pid[order][left:right]
+            row_item = row[order][left:right]
+
+            with h5.File(save_path + f"{item}.hdf5", "a") as hdf:
+                if not name in hdf.keys():
+                    hdf.create_group(name)
+                
+                # If it is the first time opening this file, create datasets
+                if not 'ID' in hdf[name].keys():
+                    hdf.create_dataset(
+                        name=f'{name}/ID',
+                        data=pid_item,
+                        maxshape=(None, ),
+                        dtype=uint_dtype_ids,
+                    )
+                    hdf.create_dataset(
+                        name=f'{name}/pos',
+                        data=pos_item,
+                        maxshape=(None, pos_item.shape[-1]),
+                        dtype=dtypes[0],
+                    )
+                    hdf.create_dataset(
+                        name=f'{name}/vel',
+                        data=vel_item,
+                        maxshape=(None, vel_item.shape[-1]),
+                        dtype=dtypes[1],
+                    )
+                    hdf.create_dataset(
+                        name=f'{name}/row_idx',
+                        data=row_item,
+                        maxshape=(None, ),
+                        dtype=uint_dtype_row,
+                    )
+                # If it is not the first time opening the file, reshape the 
+                # datasets
+                else:
+                    last_item = pid_item.shape[0]
+                    new_shape = hdf[f'{name}/ID'].shape[0] + last_item
+
+                    hdf[f'{name}/ID'].resize((new_shape), axis=0)
+                    hdf[f'{name}/ID'][-last_item:] = pid_item
+                    hdf[f'{name}/pos'].resize((new_shape), axis=0)
+                    hdf[f'{name}/pos'][-last_item:] = pos_item
+                    hdf[f'{name}/vel'].resize((new_shape), axis=0)
+                    hdf[f'{name}/vel'][-last_item:] = vel_item
+                    hdf[f'{name}/row_idx'].resize((new_shape), axis=0)
+                    hdf[f'{name}/row_idx'][-last_item:] = row_item
+
     return None
+
 
 if __name__ == '__main__':
     pass
